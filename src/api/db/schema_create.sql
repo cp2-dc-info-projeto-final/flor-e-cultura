5806cdbb9e9801abba317c483e618d0075ee73f9
ms90-3 <moisescardoso3c273advancedx@gmail.com>
2025-07-07 14:34:25 -0300

    Update schema_create.sql

--- src/api/db/schema_create.sql
+++ src/api/db/schema_create.sql
@@ -1,5 +1,3 @@
-DROP TABLE IF EXISTS usuario;
-
 CREATE TABLE usuario (
     id bigint GENERATED ALWAYS AS IDENTITY,
     login text NOT NULL,
@@ -9,3 +7,15 @@ CREATE TABLE usuario (
 INSERT INTO usuario (login, email) VALUES
 ('hermenegildo', 'hermenegildo@email.com'),
 ('zoroastra', 'zoroastra@email.com');
+
+
+CREATE IF NOT EXISTS TABLE usuarios (
+  id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  nome_completo TEXT NOT NULL,
+  email TEXT UNIQUE NOT NULL,
+  senha TEXT NOT NULL,
+  cpf VARCHAR(14) UNIQUE NOT NULL,
+  telefone VARCHAR(15) NOT NULL,
+  data_nascimento DATE NOT NULL,
+  tipo_usuario TEXT NOT NULL DEFAULT 'cliente'
+);
