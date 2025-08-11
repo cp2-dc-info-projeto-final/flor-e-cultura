b33aa7ab6c8ca2871eb7cb60d6184e27a87a27b1
Joaogabrielpereiralopes <gabriel.pitito123@gmail.com>
2025-08-11 14:57:51 -0300

    criaçao da pagina de ediçao de usuarios

--- src/frontend-app/src/routes/+layout.svelte
+++ src/frontend-app/src/routes/+layout.svelte
@@ -1,6 +1,25 @@
 <script lang="ts">
 	let { children } = $props();
 	import '../shared.css';
+	import { onMount } from 'svelte'
+onMount(() => {
+  const button = document.getElementById('dropdownDefaultButton');
+  const dropdown = document.getElementById('dropdown');
+
+  if (button && dropdown) {
+    button.addEventListener('click', () => {
+      dropdown.classList.toggle('hidden');
+    });
+
+    // Fecha ao clicar fora
+    document.addEventListener('click', (event: MouseEvent) => {
+      const target = event.target as Node;
+      if (!button.contains(target) && !dropdown.contains(target)) {
+        dropdown.classList.add('hidden');
+      }
+    });
+  }
+});
 </script>
 
 
@@ -9,8 +28,32 @@
 	<a href="bbbbb" class="flex items-center space-x-3 rtl:space-x-reverse">
 		<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-black">𝐅𝐥𝐨𝐫&𝐂𝐮𝐥𝐭𝐮𝐫𝐚</span>
 	</a>
-	<div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+	<div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse space-x-4">
 		<a href="/cadastro" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-green-400 dark:hover:bg-green-700 dark:focus:ring-blue-800">Cadastre-se</a>
+		
+		<div class="relative inline-block text-left">
+			<!-- Botão -->
+			<button id="dropdownDefaultButton"
+			  class="text-white bg-blue-700 hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-gray-500 dark:hover:bg-gray-700 dark:focus:ring-blue-800"
+			  type="button">
+			  Configurações
+			  <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+				<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+				  d="m1 1 4 4 4-4" />
+			  </svg>
+			</button>
+		  
+			<!-- Dropdown -->
+			<div id="dropdown"
+			  class="absolute z-10 hidden mt-2 bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700">
+			  <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+				<li>
+				  <a href="/editar" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Editar
+					usuário</a>
+				</li>
+			  </ul>
+			</div>
+		  </div>
 		<button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
 		  <span class="sr-only">Open main menu</span>
 		  <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -36,6 +79,8 @@
 </div>
 </div>
 </nav>
+
+
 {@render children()}
 
 
