ae5c8b5f14c871cc54d07f1c85a64f631d83284b
Joaogabrielpereiralopes <gabriel.pitito123@gmail.com>
2025-10-20 14:49:48 -0300

    reatividade pronta

---
+++ src/src/routes/editar/+page.svelte
@@ -0,0 +1,161 @@
+<script>
+  import { onMount } from 'svelte';
+
+  // Variáveis do formulário
+
+  let usuario = [{
+        id: 0,
+        nome_completo: "",
+        email: '',
+        cpf: '',
+        telefone: '',
+        data_nascimento: '',
+        tipo_usuario: 1
+      }];
+  usuario.pop()
+
+  let nome = '';
+  let email = '';
+  let dataNascimento = '';
+  let cpf = '';
+  let telefone = '';
+  let senha = '';
+  let confirmarSenha = '';
+  let erro = '';
+  let id = '';
+  let carregando = true;
+
+  async function carregarUsuario() {
+    try {
+      const res = await fetch('http://localhost:3000/users/');
+      const data = await res.json();
+
+      if (!data.success) {
+        erro = data.message || 'Erro ao buscar usuário';
+        return;
+      }
+
+      usuario = data.data;
+    } catch (e) {
+      erro = 'Erro ao conectar com o servidor';
+    }
+  }
+  
+  // Buscar os dados do usuário ao carregar a página
+  onMount(async () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    id = urlParams.get('id');
+    if (!id) {
+      alert('ID de usuário não encontrado.');
+      return;
+    }
+  
+    try {
+      const res = await fetch(`http://localhost:3000/users/${id}`);
+      const dados = await res.json();
+      const user = dados.data || dados;
+      nome = user.nome_completo;
+      email = user.email;
+      dataNascimento = user.data_nascimento;
+      cpf = user.cpf;
+      telefone = user.telefone;
+      senha = user.senha;
+      confirmarSenha = senha;
+    } catch (err) {
+      console.error('Erro ao buscar dados do usuário:', err);
+      alert('Erro ao buscar dados do usuário.');
+    }
+    finally {
+      carregando = false;
+    }
+  });
+
+  // Atualizar dados do usuário
+  async function editarUsuario() {
+    if (senha !== confirmarSenha) {
+      alert('As senhas não coincidem!');
+      return;
+    }
+
+    const usuario = {
+      nome_completo: nome,
+      email,
+      senha,
+      cpf,
+      telefone,
+      data_nascimento: dataNascimento,
+      tipo_usuario: 1
+    };
+
+    try {
+      const response = await fetch(`http://localhost:3000/users/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(usuario)
+      });
+
+      const result = await response.json();
+
+      if (response.ok && result.success) {
+        alert('Usuário atualizado com sucesso!');
+      } else {
+        alert(`Erro: ${result.message}`);
+      }
+    } catch (error) {
+      console.error('Erro ao atualizar usuário:', error);
+      alert('Erro ao conectar com o servidor.');
+    }
+  }
+  
+</script>
+
+{#if !carregando}
+<section>
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                    Edite seus dados
+                </h1>
+                <form on:submit|preventDefault={editarUsuario} class="space-y-4 md:space-y-6" action="#">
+                    <div>
+                        <label for="nome" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome</label>
+                        <input type="text" name="nome" id="nome" bind:value={nome} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Fulano Beltrano Silva" required>
+                    </div>
+                    <div>
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                        <input type="email" name="email" id="email" bind:value={email} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="nome@compania.com" required>
+                    </div>
+                    <div>
+                        <label for="data-nascimento" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Data de nascimento</label>
+                        <input type="date" name="data-nascimento" id="data-nascimento" bind:value={dataNascimento} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                    </div>
+
+                    <div>
+                        <label for="cpf" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">CPF</label>
+                        <input type="text" bind:value={cpf} name="cpf" id="cpf" placeholder="Seu CPF" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                    </div>
+                    <div>
+                        <label for="telefone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">telefone</label>
+                        <input type="text" bind:value={telefone} name="telefone" id="telefone" placeholder="Seu telefone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                    </div>
+                    <div>
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Senha</label>
+                        <input type="password" name="password" id="password" bind:value={senha}  placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                    </div>
+                    <div>
+                        <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirme sua senha</label>
+                        <input type="password" name="confirm-password" id="confirm-password" bind:value={confirmarSenha} placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                    </div>
+                    <button id="cadastro" type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Confirme seus dados</button>
+                    
+                </form>
+            </div>
+        </div>
+    </div>
+  </section>
+  {:else}
+  <p class="text-center text-white">Carregando dados do usuário...</p>
+{/if}
---
+++ src/src/routes/editarprodutos/+page.svelte
@@ -0,0 +1,158 @@
+<script>
+    import { onMount } from 'svelte';
+  
+    // Variáveis do formulário
+  
+    let produto = [{ 
+    nome_produto: "",    
+    descricao: "",
+    preco: "", 
+    quantidade: "",
+    criado_em: "",
+    atualizado_em: "",
+        }];
+    produto.pop()
+  
+    let nome_produto = '';
+    let descricao = '';
+    let preco = '';
+    let quantidade = '';
+    let criado_em = '';
+    let atualizado_em = '';
+    let erro = '';
+    let id = '';
+    let carregando = true;
+  
+    async function carregarProduto() {
+      try {
+        const res = await fetch('http://localhost:3000/produtos/');
+        const data = await res.json();
+  
+        if (!data.success) {
+          erro = data.message || 'Erro ao buscar produto';
+          return;
+        }
+  
+        produto = data.data;
+      } catch (e) {
+        erro = 'Erro ao conectar com o servidor';
+      }
+    }
+    
+    // Buscar os dados do usuário ao carregar a página
+    onMount(async () => {
+      const urlParams = new URLSearchParams(window.location.search);
+      id = urlParams.get('id');
+      if (!id) {
+        alert('ID de produto não encontrado.');
+        return;
+      }
+    
+      try {
+        const res = await fetch(`http://localhost:3000/produtos/${id}`);
+        const dados = await res.json();
+        const produto = dados.data || dados;
+        nome_produto = produto.nome_produto;
+        descricao = produto.descricao;
+        preco = produto.preco;
+        quantidade = produto.quantidade;
+        criado_em = produto.criado_em;
+        atualizado_em = produto.atualizado_em;
+      } catch (err) {
+        console.error('Erro ao buscar dados do usuário:', err);
+        alert('Erro ao buscar dados do usuário.');
+      }
+      finally {
+        carregando = false;
+      }
+    });
+  
+    // Atualizar dados do usuário
+    async function editarProduto() {
+  
+      const produto = {
+        nome_produto: nome_produto,
+        descricao,
+        preco,
+        quantidade,
+        criado_em,
+        atualizado_em,
+      };
+  
+      try {
+        const response = await fetch(`http://localhost:3000/produtos/${id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(produto)
+        });
+  
+        const result = await response.json();
+  
+        if (response.ok && result.success) {
+          alert('Produto atualizado com sucesso!');
+        } else {
+          alert(`Erro: ${result.message}`);
+        }
+      } catch (error) {
+        console.error('Erro ao atualizar produto:', error);
+        alert('Erro ao conectar com o servidor.');
+      }
+    }
+    
+  </script>
+  
+  {#if !carregando}
+  <section>
+      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-white dark:border-gray-700">
+              <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                  <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-black">
+                      Edite os dados do produto
+                  </h1>
+                  <form on:submit|preventDefault={editarProduto} class="space-y-4 md:space-y-6" action="#">
+                    <div>
+                        <label for="nome_produto" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Nome do produto</label>
+                        <input type="text" id="nome_produto" bind:value={nome_produto} required placeholder="Margarida"
+                          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 focus:ring-pink-700 focus:border-pink-700 dark:bg-pink-200 dark:border-gray-600 dark:text-black" />
+                      </div>
+            
+                      <div>
+                        <label for="descricao" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Descrição</label>
+                        <input type="text" id="descricao" bind:value={descricao} required placeholder="Planta com flor"
+                          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 focus:ring-pink-700 focus:border-pink-700 dark:bg-pink-200 dark:border-gray-600 dark:text-black" />
+                      </div>
+            
+                      <div>
+                        <label for="preco" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Preço</label>
+                        <input type="string" id="preco" bind:value={preco} required
+                          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 focus:ring-pink-700 focus:border-pink-700 dark:bg-pink-200 dark:border-pink-600 dark:text-black" />
+                      </div>
+            
+                      <div>
+                        <label for="quantidade" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Quantidade</label>
+                        <input type="text" id="quantidade" bind:value={quantidade} required placeholder="10"
+                          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 focus:ring-pink-700 focus:border-pink-700 dark:bg-pink-200 dark:border-gray-600 dark:text-black" />
+                      </div>
+            
+                      <div>
+                        <label for="criado_em" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Criado em:</label>
+                        <input type="date" id="criado_em" bind:value={criado_em} required placeholder="00/00/0000"
+                          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 focus:ring-pink-700 focus:border-pink-700 dark:bg-pink-200 dark:border-gray-600 dark:text-black" />
+                      </div>
+          
+                      <div>
+                          <label for="atualizado_em" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Atualizado em:</label>
+                          <input type="date" id="atualizado_em" bind:value={atualizado_em} required placeholder="00/00/0000"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 focus:ring-pink-700 focus:border-pink-700 dark:bg-pink-200 dark:border-gray-600 dark:text-" />
+                        </div>
+                        <button id="cadastro" type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-400 dark:hover:bg-green-700 dark:focus:ring-green-800">Confirme seus dados</button>
+                  </form>
+              </div>
+          </div>
+      </div>
+    </section>
+    {:else}
+    <p class="text-center text-white">Carregando dados da planta...</p>
+  {/if}
